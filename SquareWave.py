244ff520fe4c7020bce1e837df954c8978351c82
SK HAPIJUL HOSSEN <skhapijulhossen@gmail.com>
2021-05-03 00:24:39 +0530

    Square wave using some of sinusoids

---
+++ SquareWave.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Sn():
+    ""
+    global n  
+    plt.figure(figsize=(6,4))
+    plt.plot([0,1],[0,0], color="grey",linewidth=3)
+
+    t = np.linspace(0,1,100)
+    y = np.zeros(100)
+    for n in range(1,n,2):
+        sin = np.sin(2*np.pi*n*t)/n
+        y += sin
+    y = (y*4)/np.pi
+    plt.plot(t,y , color="orange")
+    plt.grid(True, color="lightgrey")
+    plt.savefig("squareWave.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    n = int(input("Enter Number of Harmonics should be <=11: "))
+    if n <= 11:
+        Sn()
+    else:
+        print("Entered Number of Harmonics is Greater than 11!")
